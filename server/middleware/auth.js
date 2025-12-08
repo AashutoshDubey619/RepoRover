bfb19acdabc07d247ff5bfbdf076a2d1793fb1f9
Aashutosh Dubey <kailashdubey@outlook.com>
2025-12-08 18:47:44 +0530

    Bugs fixed

--- server/middleware/auth.js
+++ server/middleware/auth.js
@@ -15,7 +15,7 @@ const auth = async (req, res, next) => {
         const isCustomAuth = token.length < 500;
 
         if (token && isCustomAuth) {
-            const decodedData = jwt.verify(token, 'SECRET_KEY');
+            const decodedData = jwt.verify(token, process.env.JWT_SECRET || 'SECRET_KEY');
 
             req.userId = decodedData?.id;
         } else {
